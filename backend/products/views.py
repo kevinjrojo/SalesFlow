efaf34336ab251964978bcd57acd9caa98a45986
Jose <josemoyano059@gmail.com>
2025-05-11 02:18:08 -0300

    Agregar vistas para los modelos Unit, ProductCategory y Product en la aplicación products

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from rest_framework import response as res, request as req, viewsets, views, filters
+from products import models, serializers
 
 # Create your views here.
+class UnitViewSet(viewsets.ModelViewSet):
+    """
+    Urls:
+    /product/unit/ -> GET (all), POST
+    /product/unit/<int:pk>/ -> GET, PUT, DELETE
+    """
+    
+    queryset = models.Unit.objects.all()
+    serializer_class = serializers.UnitSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name', 'abbreviation']
+    
+class ProductCategoryViewSet(viewsets.ModelViewSet):
+    """
+    Urls:
+    /product/category/ -> GET (all), POST
+    /product/category/<int:pk>/ -> GET, PUT, DELETE
+    """
+    queryset =  models.ProductCategory.objects.all()
+    serializer_class = serializers.ProductCategorySerializer
+    
+
+class ProductView(viewsets.ViewSet):
+    """
+    Urls:
+    /product/ -> GET (all), POST
+    /product/<int:pk>/ -> GET, PUT, DELETE
+    """
+    
+    queryset = models.Product.objects.all()
+    serializer_class = serializers.ProductSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name', 'gtin']
+    
