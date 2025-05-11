13f809cc089c3adc9b30f693b21ce4180839e7a6
Jose <josemoyano059@gmail.com>
2025-05-11 04:16:30 -0300

    Agregar lógica de activación/desactivación en el modelo Product y mejorar serializador para incluir precios y relaciones

--- backend/products/models.py
+++ backend/products/models.py
@@ -9,15 +9,25 @@ class Product(models.Model):
     stock = models.DecimalField(default=0, max_digits=10, decimal_places=3)
     unit = models.ForeignKey('products.Unit', on_delete=models.SET_NULL, null=True)
     category = models.ForeignKey('products.ProductCategory', on_delete= models.SET_NULL, null=True)
+    unit_price = models.DecimalField(max_digits=100, decimal_places=2)
+    cost_price = models.DecimalField(max_digits=100, decimal_places=2)
     is_active = models.BooleanField(default=True)
+
+    def activate(self) -> None: self.is_active = True
+    def deactivate(self) -> None: self.is_active = False
 
 class Unit(models.Model):
     name = models.CharField(max_length=100, unique=True)
     abbreviation = models.CharField(max_length=10, null=True, unique=True)
     is_divisible = models.BooleanField(default=False)
     
+    def __str__(self):
+        return f"{self.name} [{self.abbreviation}]"
+    
     
 class ProductCategory(models.Model):
     name = models.CharField(max_length=100, unique=True)
     prefix = models.CharField(max_length=5, null=True, unique=True)
     
+    def __str__(self):
+        return self.name
