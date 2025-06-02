e0d24e216b91d3bb0fb56b76c7d0f8faba7c34e1
Jose <josemoyano059@gmail.com>
2025-06-01 23:10:27 -0300

    feat: Estilos de Header

--- frontend/src/component/pages/box/Header.jsx
+++ frontend/src/component/pages/box/Header.jsx
@@ -1,16 +1,42 @@
-import user from "../../../assets/avatar.png";
-
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { UserIcon } from "../../icons/UserIcon";
+import { ArrowDownIcon } from "../../icons/ArrowDownIcon";
 import "../../../styles/header.css";
 
-export const Header = () => {
+export const Header = ({ username = "user", userId = "12345678" }) => {
+  const location = useLocation();
+  const [pageTitle, setPageTitle] = useState();
+
+  const paths = {
+    home: "/home",
+    cashRegister: "/cash_register",
+    products: "/products",
+    reports: "/reports",
+  };
+  const currentPath = location.pathname;
+  const isActive = (path) => currentPath.startsWith(path);
+
+  useEffect(() => {
+    if (isActive(paths.home)) setPageTitle("Inicio");
+    else if (isActive(paths.cashRegister)) setPageTitle("Caja");
+    else if (isActive(paths.products)) setPageTitle("Productos");
+    else if (isActive(paths.reports)) setPageTitle("Informes");
+    else setPageTitle("");
+  }, [currentPath]);
+
   return (
     <div className="header">
-      <section>
-        <h3>Caja</h3>
-      </section>
-      <div>
-        <img src={user} alt="" />
-        <strong>usuario</strong>
+      <h3 className="page__title">{pageTitle}</h3>
+
+      <div className="user">
+        <span className="separator" />
+        <UserIcon className="user__icon" />
+        <div className="user__data">
+          <span className="username">{username}</span>
+          <span className="user__id">{userId}</span>
+        </div>
+        <ArrowDownIcon className="arrow" />
       </div>
     </div>
   );
